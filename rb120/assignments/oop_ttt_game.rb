19ccdb8697e51cafb15ce3778ed630253c058c35
smualimc <88353918+smualimc@users.noreply.github.com>
2022-08-25 13:39:36 +0200

    Initial commit

---
+++ rb120/assignments/oop_ttt_game.rb
@@ -0,0 +1,247 @@
+module Displayable
+    def prompt(message)
+      puts "> #{message}"
+    end
+  
+    def clear_screen
+      system 'clear'
+    end
+  
+    def beep
+      puts 7.chr
+    end
+
+    def skip
+      puts ""
+    end
+  
+  end
+  class Players
+    attr_accessor :mark, :name, :sequence
+    COMPUTER_NAMES = %w(Robocop Terminator MyRobot Robottina)
+  
+    def initialize
+      @sequence = []
+      settings
+    end
+  
+  end
+  
+  class Human < Players
+  include Displayable
+  
+    def settings
+      set_name
+      set_mark
+    end
+  
+    def set_mark
+      prompt("As your game mark would you want X or O:")
+      skip
+      option = ''
+      loop do
+        option = gets.chomp.upcase
+        beep
+        break if %w(X O).include? option
+        prompt("Sorry, wrong option, just enter X or O")
+      end
+      self.mark = option
+    end
+  
+    def set_name
+      prompt("Please enter your name:")
+      skip
+      input = ''
+      loop do
+        input = gets.chomp
+        skip
+        break unless input.empty?
+        prompt("Sorry, you must provide a player name")
+      end
+      self.name = input
+    end
+  
+  
+  end
+  
+  class Computer < Players
+    include Displayable
+  
+    def settings
+      set_mark
+      set_name
+    end
+  
+    def set_mark
+      self.mark = (human.mark == 'X' ?  'O' : 'X')
+    end
+  
+    def set_name
+      self.name = Players::COMPUTER_NAMES.sample
+      beep
+    end
+  
+  end
+  
+  class Game
+    include Displayable
+  
+    def initialize_grid
+      board = {}
+      (1..9).each {|key| board[key] = ' '}
+      board
+    end
+  
+    grid = {1 => ' ', 2 => ' ', 3 => ' ', 4 => ' ', 5 => ' ', 6 => ' ', 7 => ' ', 8 => ' ', 9 => ' '}
+  
+    def display_grid!(grid)
+      "  |     |     |     |
+      |  #{grid[1]}  |  #{grid[2]}  |  #{grid[3]}  |
+      |     |     |     |
+      |-----|-----|-----|
+      |     |     |     |
+      |  #{grid[4]}  |  #{grid[5]}  |  #{grid[6]}  |
+      |     |     |     |
+      |-----|-----|-----|
+      |     |     |     |
+      |  #{grid[7]}  |  #{grid[8]}  |  #{grid[9]}  |
+      |     |     |     |"
+    end
+  
+    def show_squares
+      prompt("To play you'll have to put your mark on one of the following square numbers:")
+      skip
+      puts "| 1 | 2 | 3 |"
+      puts "|---|---|---|"
+      puts "| 4 | 5 | 6 |"
+      puts "|---|---|---|"
+      puts "| 7 | 8 | 9 |"
+      skip
+    end
+  
+    def get_available_squares(grid)
+      grid.keys.select { |_,value| value == ' ' }
+    end
+  
+    def human_moves!(grid)
+      available_array = get_available_squares(grid)
+      prompt("Select one available square from: #{available_array}")
+      selection = ''
+      loop do
+        selection = gets.chomp
+        break if selection.match?(/^\d$/) && available_array.include?(selection)
+        prompt("Wrong selection, try again please")
+      end
+      grid[selection] = self.mark
+      self.sequence << selection
+    end
+  
+    def computer_marks!(grid)
+      selection = get_available_squares.sample
+      grid[selection] = self.mark
+      self.sequence << selection
+    end
+  
+    def marking!(player, grid)
+      player.class = Human ? human_marks!(grid) : computer_marks!(grid)
+  
+    end
+  
+    def get_grid_state
+  
+    end
+  
+    def winner_or_tie?
+  
+    end
+  
+    def display_winner
+  
+    end
+  
+    def separating_line
+      puts "|----------------|----------------|"
+    end
+  
+    def names_line
+      puts "| #{human.name}    | #{computer.name}      |"
+    end
+  
+    def marks_lines
+      puts "| #{human.mark}    | #{computer.mark}      |"
+    end
+  
+    def human_marking_line
+      markings = human.sequence.size
+      (0...markings-1).each do |index|
+        puts "| #{human.sequence[index]}    |  #{computer.sequence[index]}     |"
+        separating_line
+      end
+      puts "| #{human.sequence.last}    |       |"
+      separating_line
+    end
+  
+    def computer_marking_line
+      markings = computer.sequence.size
+      (0...markings).each do |index|
+        puts "| #{human.sequence[index]}    |  #{computer.sequence[index]}     |"
+        separating_line
+      end
+    end
+      
+    def show_marking(player)
+      separating_line
+      names_line
+      marks_lines
+      separating_line
+      player.class == Human ? human_marking_line : computer_marking_line
+  
+    end
+  
+    def play_again?
+  
+    end
+  
+    def display_end_message
+  
+    end
+  
+    def display_welcome_message
+      clear_screen
+      skip
+      prompt("Welcome to the Tic Tac Toe game")
+      skip
+    end
+  
+    def play
+      display_welcome_message
+      show_squares
+      human = Human.new
+      computer = Computer.new
+      loop do
+        grid = initialize_grid 
+        display_grid!(grid)
+        loop do
+          marking!(human, grid)
+          show_marking(human)
+          display_grid!(grid)
+          if winner_or_tie?
+            display_winner
+            break
+          end
+          marking!(computer, grid)
+          show_marking
+          display_grid_state
+          if winner_or_tie?
+            display_winner
+            break
+          end
+        end
+        break unless play_again?
+      end
+      display_end_message
+    end
+  end
+  
+  Game.new.play
+    
